c05a537a3de6c4fcc650d760a32745e3fa8caf11
dankazim <itskazimz@gmail.com>
2021-03-15 20:08:22 +0700

    updated the views, model, and templates

--- Notes/models.py
+++ Notes/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 class NoteBook(models.Model):
-    question_text = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    plain_note = models.TextField(blank=True)
     
     def __str__ (self):
-    
-        return "ID: {} Question text:{} Pub Date: {}".format(self.id,self.question_text,self.pub_date)
+        return "{} ".format(self.title)
--- Notes/views.py
+++ Notes/views.py
@@ -5,11 +5,21 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from .models import NoteBook
 
 # Create your views here.
-def create_note(request):
-    title = request.POST['title']
-    content = request.POST['content']
-   # note = Note(title=title, content=content, doc=timezone.now())
-    #note.save()
-    return HttpResponseRedirect(reverse('note_keeper:index'))
+
+class IndexView(generic.ListView):
+    template_name = 'Notes/index.html'
+    context_object_name = 'Notes_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return NoteBook.objects.order_by('pub_date')[:5]
+
+# def create_note(request):
+#     title = request.POST['title']
+#     plain_note = request.POST['content']
+#    # note = Note(title=title, content=content, doc=timezone.now())
+#     #note.save()
+#     return HttpResponseRedirect(reverse('note_keeper:index'))
