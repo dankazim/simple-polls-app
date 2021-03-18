5197d733d031c482c7189823613e4990ba9598bf
Dan <khazifire@gmail.com>
2021-03-18 18:02:06 +0700

    added favicon

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('Notes/', include('Notes.urls')),
+    path('', include('Notes.urls')),
 ]
