ffd64dad15435d153d6b5b4e2fbd862e34e504e3
dankazim <itskazimz@gmail.com>
2021-03-19 08:42:07 +0700

    updated model

--- Notes/models.py
+++ Notes/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class NoteBook(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True, blank=True)
+    pub_date = models.DateTimeField('date published',auto_now_add=True, blank=True)
     plain_note = models.TextField(blank=True)
-    
+    update_date = models.DateTimeField('date Updated', default=timezone.now())
     def __str__ (self):
         return "{}".format(self.title)
