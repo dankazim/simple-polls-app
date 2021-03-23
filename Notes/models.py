e0029f8ebc5fdd8bdcdd4db21159793e0fd69fa0
dankazim <itskazimz@gmail.com>
2021-03-23 16:33:39 +0700

    conducted the test

--- Notes/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class NoteBook(models.Model):
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now=True)
-    plain_note = models.TextField(blank=True)
-    def __str__ (self):
-        return "{}".format(self.title)
--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     def __str__ (self):
-    
-        return "ID: {} Question text:{} Pub Date: {}".format(self.id,self.question_text,self.pub_date)
+        return "{}".format(self.question_text)
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
